8a49f14afcd7a182b8522bdb5244d1b85e1c8309
wadwax <22623048+wadwax@users.noreply.github.com>
2022-08-25 22:15:33 +0900

    migrate to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
