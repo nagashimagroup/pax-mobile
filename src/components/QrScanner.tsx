0380c105acf5a6977e9c3e4149906e91866b2dd9
wadwax <22623048+wadwax@users.noreply.github.com>
2022-08-30 15:48:35 +0900

    added qr code scanner

---
+++ src/components/QrScanner.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { forwardRef, useCallback, useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import QrScanner from "qr-scanner";
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="left" ref={ref} {...props} />;
+});
+
+interface QrReaderProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export default function QrReader({ open, setOpen }: QrReaderProps) {
+  const [scanner, setScanner] = useState<any>(null);
+
+  const handleClose = () => {
+    setOpen(false);
+    scanner?.stop();
+  };
+
+  const startScan = useCallback((videoRef: HTMLVideoElement) => {
+    if (!videoRef) return;
+
+    const qrScanner = new QrScanner(
+      videoRef,
+      (result: any) => {
+        console.log({ result });
+        qrScanner.stop();
+        setOpen(false);
+      },
+      { highlightScanRegion: true, highlightCodeOutline: true }
+    );
+
+    setScanner(qrScanner);
+
+    qrScanner.start();
+  }, []);
+
+  return (
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Transition}
+    >
+      <AppBar sx={{ position: "relative" }}>
+        <Toolbar>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            QRコード
+          </Typography>
+          <IconButton color="inherit" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <div className="fixed inset-0 bg-black overflow-hidden object-cover">
+        <video className="h-screen mx-auto" ref={startScan} />
+      </div>
+    </Dialog>
+  );
+}
--- src/layouts/AppBar.tsx
+++ src/layouts/AppBar.tsx
@@ -1,4 +1,5 @@
 import { useProject } from "contexts/project";
+import { useState } from "react";
 import Link from "next/link";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,14 +9,17 @@ import AccountIcon from "@mui/icons-material/AccountCircle";
 import HomeIcon from "@mui/icons-material/Home";
 import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
 import { useAuth } from "contexts/auth";
+import QrScanner from "components/QrScanner";
 
 interface PaxAppBarProps {
   open?: boolean;
   toggleDrawer: () => void;
 }
 
-export default function PaxAppBar({ open, toggleDrawer }: PaxAppBarProps) {
+export default function PaxAppBar({ toggleDrawer }: PaxAppBarProps) {
   const { currentGroup } = useAuth();
+  const [showQrScan, setShowQr] = useState(false);
+
   if (!currentGroup)
     return (
       <div className="h-screen flex justify-center items-center">
@@ -24,67 +28,74 @@ export default function PaxAppBar({ open, toggleDrawer }: PaxAppBarProps) {
     );
 
   return (
-    <AppBar>
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={toggleDrawer}
-          sx={{
-            marginRight: "1rem",
-          }}
-        >
-          <AccountIcon />
-        </IconButton>
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          sx={{ flexGrow: 1 }}
-        >
-          {currentGroup.name}
-        </Typography>
-        <IconButton color="inherit" onClick={() => null}>
-          <QrCodeScannerIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <>
+      <QrScanner open={showQrScan} setOpen={setShowQr} />
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "1rem",
+            }}
+          >
+            <AccountIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            {currentGroup.name}
+          </Typography>
+          <IconButton color="inherit" onClick={() => setShowQr(true)}>
+            <QrCodeScannerIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
 export function ProjectAppBar() {
   const { project } = useProject();
+  const [showQrScan, setShowQr] = useState(false);
 
   return (
-    <AppBar>
-      <Toolbar>
-        <Link href="/">
-          <IconButton
-            edge="start"
+    <>
+      <QrScanner open={showQrScan} setOpen={setShowQr} />
+      <AppBar>
+        <Toolbar>
+          <Link href="/">
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="back"
+              sx={{
+                marginRight: "1rem",
+              }}
+            >
+              <HomeIcon />
+            </IconButton>
+          </Link>
+          <Typography
+            component="h1"
+            variant="h6"
             color="inherit"
-            aria-label="back"
-            sx={{
-              marginRight: "1rem",
-            }}
+            noWrap
+            sx={{ flexGrow: 1 }}
           >
-            <HomeIcon />
+            {project?.name}
+          </Typography>
+          <IconButton color="inherit" onClick={() => setShowQr(true)}>
+            <QrCodeScannerIcon />
           </IconButton>
-        </Link>
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          sx={{ flexGrow: 1 }}
-        >
-          {project?.name}
-        </Typography>
-        <IconButton color="inherit" onClick={() => null}>
-          <QrCodeScannerIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
